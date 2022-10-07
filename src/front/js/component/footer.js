9769e365c4bb9632c466d9140adcb1aa5de8526d
sibelius303 <cesarev17@gmail.com>
2022-10-07 19:11:37 +0000

    prototipo de footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,25 @@
-import React, { Component } from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export default function FooterApp() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+	<div className='footer'>
+	<Box sx={{ width: 500 }}>
+      <BottomNavigation className='footerBox'
+      >
+        <BottomNavigationAction label="Recents" icon={<FacebookIcon/>} />
+        <BottomNavigationAction label="Favorites" icon={<InstagramIcon/>} />
+        <BottomNavigationAction label="Nearby" icon={<TwitterIcon/>} />
+      </BottomNavigation>
+    </Box>
+	</div>
+    
+  );
+};
