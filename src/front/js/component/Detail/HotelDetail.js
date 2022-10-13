e0d0a20fb5002b772efc274f88986a98a8213e68
Jose Daniel Cacioppo Armas <danielcacioppo@gmail.com>
2022-10-13 01:01:26 +0000

    base

---
+++ src/front/js/component/Detail/HotelDetail.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from "react";
+import NavbarCiap from "./NavbarCiap";
+
+const HotelDetail = (props) => {
+  // const {
+  //   onChange,
+  //   data: { id, description, done },
+  // } = props;
+
+  return (
+    <>
+      <NavbarCiap></NavbarCiap>
+    </>
+  );
+};
+
+export default HotelDetail;
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,29 +9,19 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import HotelDetail from "./component/Detail/HotelDetail";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <HotelDetail></HotelDetail>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
